e9232812b9a55802f400020cd4227d6ac2b3513b
thomasgwatson <thomasgabriel.watson@gmail.com>
2013-11-12 11:18:58 -0800

    Changed routes to show the top ten tweets of a given user

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,25 @@
 get '/' do
+  # Twitter.update('tweeting from sinatra')
   # Look in app/views/index.erb
   erb :index
+end
+
+get '/:username' do
+  # params[:username]
+  # find a way to grab a user's tweets
+  # tweet = Twitter.user_timeline(params[:username], {count: 1})
+  # "here are #{params[:username]}'s tweets"
+  # p tweet.first.id
+
+  # "https://twitter.com/GabbyMcNabb/status/400333249039183872"
+  prefix = "https://twitter.com/"
+  output = ""
+  tweets = Twitter.user_timeline(params[:username], {count: 10})
+
+  tweets.each do |t|
+    url = "#{prefix}/#{t.user.screen_name}/status/#{t.id}"
+    output += "<a target='_new' href='#{url}'>#{t.text}</a><br>"
+  end
+
+  output
 end
--- config/environment.rb
+++ config/environment.rb
@@ -46,4 +46,17 @@ Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
 
+env_config = YAML.load_file(APP_ROOT.join('config', 'twitter.yaml'))
+
+env_config.each do |key, value|
+  ENV[key] = value
+end
+
+Twitter.configure do |config|
+  config.consumer_key = ENV['TWITTER_KEY']
+  config.consumer_secret = ENV['TWITTER_SECRET']
+  config.oauth_token = ENV['OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['OAUTH_TOKEN_SECRET']
+end
+
 
