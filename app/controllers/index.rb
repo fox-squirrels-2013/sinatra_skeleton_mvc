52f4e1687dc0a39c70f4a7413ce6f2a5d70ebb10
thomasgwatson <thomasgabriel.watson@gmail.com>
2013-11-12 19:31:57 -0800

    Setup up DB thingos to store tweets and twitter users.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,20 +6,18 @@ end
 
 get '/:username' do
   # params[:username]
-  # find a way to grab a user's tweets
-  # tweet = Twitter.user_timeline(params[:username], {count: 1})
-  # "here are #{params[:username]}'s tweets"
-  # p tweet.first.id
+  tweet_links(params[:username])
+end
 
-  # "https://twitter.com/GabbyMcNabb/status/400333249039183872"
+
+def tweet_links(twitter_name)
   prefix = "https://twitter.com/"
   output = ""
-  tweets = Twitter.user_timeline(params[:username], {count: 10})
+  tweets = Twitter.user_timeline(twitter_name, {count: 10})
 
   tweets.each do |t|
     url = "#{prefix}/#{t.user.screen_name}/status/#{t.id}"
     output += "<a target='_new' href='#{url}'>#{t.text}</a><br>"
   end
-
   output
 end
